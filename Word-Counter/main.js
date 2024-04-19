dabbef7f26ceedcbe173c2f1d6699e2a7bb56a45
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-04-19 20:19:43 +0500

    Add files via upload

---
+++ Word-Counter/main.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        message: "Enter Your Sentence To Count The Word",
+        type: "input"
+    }
+]);
+const words = answer.sentence.toUpperCase().trim().split(" ");
+console.log(chalk.blueBright(words));
+console.log(chalk.magenta `Your Sentence Word Counte Is ${words.length}`);
